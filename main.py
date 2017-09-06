2f32407fc81fd0585658f63f3b09b8fd02ff2f45
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-09-06 10:54:05 +0200

    Pycharm

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #### Filippo Pedrazzini, David Tomic
+#### 5G and Deep Learning
+
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Input
+from keras.models import Model
 
 
 
-print("ciao")
+
+
